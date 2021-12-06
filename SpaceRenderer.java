6af62da6cb620aa6b240d169e05e656696b5f422
opelly27 <opelly@gmail.com>
2021-12-06 14:39:33 -0800

    .

---
+++ SpaceRenderer.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+public class SpaceRenderer implements Runnable {
+    
+    Space workingSpace;
+
+    public SpaceRenderer(Space inSpace) {
+        workingSpace = inSpace;
+    }
+
+    public void run(){
+        
+    }
+
+    public static void main(String[] args){
+
+    }
+}
